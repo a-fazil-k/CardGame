a81ccc3af3ced30e8c14abb585df6c4bf0e53b91
a-fazil-k <f241229057@ktun.edu.tr>
2026-05-14 17:25:57 +0300

    fix: GameManager kodu güncellendi

--- src/GameManager.cs
+++ src/GameManager.cs
@@ -1,34 +1,26 @@
 using System;
 using System.Collections.Generic;
 
-// 1. ÜRÜN SOYUTLAMASI: Tüm kartların ortak özellikleri
+// --- FAZ 1: YARATIMSAL (FACTORY METHOD) ---
 public abstract class Card 
 {
     public string Name { get; set; }
     public string PlacedPosition { get; set; }
     public float BasePower { get; set; }
     
-    // Her kart kendi tipini bilecek
     public abstract string GetPlayerType(); 
+    
+    // Güç hesaplaması için eklendi (Decorator burada devreye girecek)
+    public virtual float GetPower() 
+    {
+        return BasePower;
+    }
 }
 
-// 2. SOMUT ÜRÜNLER: Alt kart sınıfları
-public class GoalkeeperCard : Card 
-{
-    public override string GetPlayerType() => "Kaleci";
-}
+public class GoalkeeperCard : Card { public override string GetPlayerType() => "Kaleci"; }
+public class DefenderCard : Card { public override string GetPlayerType() => "Defans"; }
+public class StrikerCard : Card { public override string GetPlayerType() => "Forvet"; }
 
-public class DefenderCard : Card 
-{
-    public override string GetPlayerType() => "Defans";
-}
-
-public class StrikerCard : Card 
-{
-    public override string GetPlayerType() => "Forvet";
-}
-
-// 3. FABRİKA SINIFI (Factory Method): Kart üretim merkezi
 public class CardFactory 
 {
     public static Card CreateCard(string type, string name, float basePower) 
@@ -40,23 +32,85 @@ public class CardFactory
             "Forvet" => new StrikerCard(),
             _ => throw new ArgumentException("Geçersiz kart tipi!")
         };
-
         newCard.Name = name;
         newCard.BasePower = basePower;
         return newCard;
     }
 }
 
-// ARTIK TEMİZLENMİŞ OYUN YÖNETİCİSİ
+// --- FAZ 2: YAPISAL ÖRÜNTÜLER ---
+
+// 1. DECORATOR (Dekoratör): Mevki cezalarını if-else'siz dinamik hesaplamak için
+public abstract class CardDecorator : Card
+{
+    protected Card _card;
+
+    public CardDecorator(Card card)
+    {
+        _card = card;
+        this.Name = card.Name;
+        this.PlacedPosition = card.PlacedPosition;
+        this.BasePower = card.BasePower;
+    }
+
+    public override string GetPlayerType() => _card.GetPlayerType();
+    public override float GetPower() => _card.GetPower();
+}
+
+public class PositionPenaltyDecorator : CardDecorator
+{
+    public PositionPenaltyDecorator(Card card) : base(card) { }
+
+    public override float GetPower()
+    {
+        float power = base.GetPower();
+        
+        // Kendi mevkisinde oynamıyorsa %60 ceza alır (Spagetti if-else'ler yerine tek kural)
+        if (this.PlacedPosition != this.GetPlayerType())
+        {
+            power *= 0.6f;
+        }
+        return power;
+    }
+}
+
+// 2. FACADE (Cephe): Karmaşık oyun motorunu tek ve basit bir arayüze indirme
+public class MatchFacade
+{
+    private List<Card> _playerCards = new List<Card>();
+
+    public void SetupTeam()
+    {
+        // Kartları Fabrikadan üretip, anında Dekoratör ile sarmalıyoruz
+        var player1 = CardFactory.CreateCard("Forvet", "Oyuncu 1", 80);
+        player1.PlacedPosition = "Defans"; // Yanlış mevki (Ceza alacak)
+        _playerCards.Add(new PositionPenaltyDecorator(player1));
+
+        var player2 = CardFactory.CreateCard("Kaleci", "Oyuncu 2", 90);
+        player2.PlacedPosition = "Kaleci"; // Doğru mevki (Tam güç)
+        _playerCards.Add(new PositionPenaltyDecorator(player2));
+    }
+
+    public void CalculateAndShowResult()
+    {
+        float totalPower = 0;
+        foreach (var card in _playerCards)
+        {
+            // Artık ceza hesabını GameManager yapmıyor, kart kendi gücünü hesaplıyor!
+            totalPower += card.GetPower(); 
+        }
+        Console.WriteLine("Takımın Toplam Gücü: " + totalPower);
+    }
+}
+
+// ARTIK TERTEMİZ OLAN ANA SINIF
 public class GameManager 
 {
-    public List<Card> PlayerCards = new List<Card>();
-
     public void StartMatch() 
     {
-        // GameManager artık "nasıl" üretileceğini bilmiyor, sadece Fabrika'dan istiyor.
-        PlayerCards.Add(CardFactory.CreateCard("Forvet", "Oyuncu 1", 80));
-        PlayerCards.Add(CardFactory.CreateCard("Kaleci", "Oyuncu 2", 90));
-        PlayerCards.Add(CardFactory.CreateCard("Defans", "Oyuncu 3", 75));
+        // GameManager sadece Facade'a emir veriyor. Gerisiyle ilgilenmiyor.
+        MatchFacade match = new MatchFacade();
+        match.SetupTeam();
+        match.CalculateAndShowResult();
     }
 }
